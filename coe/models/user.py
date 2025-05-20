05b6439b808c2b09335757b22e92a247c14ce4b8
gaurav Gusain <gaurav.g@incubyte.co>
2025-05-20 13:42:29 +0530

    DB setup with Alembic

---
+++ coe/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Text
+from coe.db.session import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(255), nullable=False)
+    last_name = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    password = Column(Text, nullable=False)
--- config.py
+++ config.py
@@ -2,7 +2,18 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "COE FastAPI App"
-    debug: bool = True
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: int
+    db_name: str
+
+    @property
+    def database_url(self):
+        return (
+            f"postgresql+psycopg2://{self.db_user}:{self.db_password}"
+            f"@{self.db_host}:{self.db_port}/{self.db_name}"
+        )
 
     class Config:
         env_file = ".env"
