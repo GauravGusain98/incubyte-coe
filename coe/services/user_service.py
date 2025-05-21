a2ad39c2320a60d5df9c9bd068fe660bf045a23f
gaurav Gusain <gaurav.g@incubyte.co>
2025-05-21 19:32:21 +0530

    Implemented password hashing

--- coe/services/user_service.py
+++ coe/services/user_service.py
@@ -1,13 +1,16 @@
 from sqlalchemy.orm import Session
 from coe.models.user import User
 from coe.schemas.user import CreateUser, UserLogin, UpdateUser
+import bcrypt
 
 def create_user(user: CreateUser, db: Session) -> User:
+    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    
     db_user = User(
         first_name=user.first_name,
         last_name=user.last_name,
         email=user.email, 
-        password=user.password
+        password=hashed_password
     )
     db.add(db_user)
     db.commit()
@@ -15,12 +18,12 @@ def create_user(user: CreateUser, db: Session) -> User:
     return db_user
 
 def login_user(login_cred: UserLogin, db: Session) -> User | None:
-    user = db.query(User).filter_by(
-        email=login_cred.email,
-        password=login_cred.password
-    ).first()
+    user = db.query(User).filter_by(email=login_cred.email).first()
 
-    return user
+    if bcrypt.checkpw(login_cred.password.encode(), user.password.encode()):
+        return user
+    
+    return None
 
 def update_user(user_id: int, user_data: UpdateUser, db: Session) -> bool:
     user = db.query(User).filter_by(id=user_id).first()
@@ -31,6 +34,8 @@ def update_user(user_id: int, user_data: UpdateUser, db: Session) -> bool:
     # Use dict and setattr to dynamically update only non-None fields
     update_fields = user_data.model_dump(exclude_unset=True, exclude={"id"})
     for field, value in update_fields.items():
+        if field == 'password':
+            value = bcrypt.hashpw(value.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         setattr(user, field, value)
 
     db.commit()
