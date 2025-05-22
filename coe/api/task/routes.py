dc52b4d20918a1a88356b57ee73cca2c670fb893
gaurav Gusain <gaurav.g@incubyte.co>
2025-05-22 10:26:24 +0530

    secure the task API

--- coe/api/task/routes.py
+++ coe/api/task/routes.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from coe.db.session import SessionLocal
+from coe.services.auth_service import get_current_user
 from coe.services.task_service import create_task, find_task_by_id, update_task_details, remove_task
 from coe.schemas.task import CreateTaskRequestSchema, CreateTaskResponseSchema, GetTaskResponseSchema, ErrorResponse, UpdateTaskRequestSchema, UpdateTaskResponseSchema, DeleteTaskResponseSchema
 
-router = APIRouter(tags=["Tasks"], prefix="/task")
+router = APIRouter(tags=["Tasks"], prefix="/task", dependencies=[Depends(get_current_user)])
 
 def get_db():
     db = SessionLocal()
